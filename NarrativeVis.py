e7531bb1081979dc6a1c1e5179bfc95cd0598ef3
nathanielliganor <145381791+nathanielliganor@users.noreply.github.com>
2024-04-26 19:00:21 -0400

    Update NarrativeVis.py

--- NarrativeVis.py
+++ NarrativeVis.py
@@ -6,9 +6,7 @@ import calendar
 # Enable data transformer for Altair
 alt.data_transformers.enable("default")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+df = pd.read_csv("./MarketData.csv")
 
 # Data preprocessing
 df['Date'] = pd.to_datetime(df['Date'])
